35c4c450900c33bf62a6fda36ec541c4a3794ddc
imcikkr <imkorean7@gmail.com>
2025-07-31 00:35:25 +0900

    컴파일 에러 수정

--- build/esp-idf/esp_driver_sdio/cmake_install.cmake
+++ build/esp-idf/esp_driver_sdio/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/lvgl_porting")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/esp32s3_4.3b_lvgl")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
--- main/main.c
+++ main/main.c
@@ -5,13 +5,27 @@
  */
 
 #include "waveshare_rgb_lcd_port.h"
+#include "ui.h"
+
+
+void func_countUp(lv_event_t *e)
+{
+    // Your code here 러하러ㅣㅓㅣㅇ라ㅓ히ㅏㅣ렇리ㅏㄹ하ㅣㅓ라ㅣ;ㅓ히ㅏㅁㄴㅇ리;ㅎ리;ㅓㅎㄹ;ㅐ
+    lv_label_set_text(ui_LblScreen, "button1 pressed");
+}
+
+void func_countDown(lv_event_t *e)
+{
+    // Your code here
+    lv_label_set_text(ui_LblScreen, "button2 pressed");
+}
 
 void app_main()
 {
     waveshare_esp32_s3_rgb_lcd_init(); // Initialize the Waveshare ESP32-S3 RGB LCD 
     // wavesahre_rgb_lcd_bl_on();  //Turn on the screen backlight 
     // wavesahre_rgb_lcd_bl_off(); //Turn off the screen backlight 
-    
+   
     ESP_LOGI(TAG, "Display LVGL demos");
     // Lock the mutex due to the LVGL APIs are not thread-safe
     if (lvgl_port_lock(-1)) {
@@ -19,12 +33,13 @@ void app_main()
         // lv_demo_benchmark();
         // lv_demo_music();
 #if CONFIG_EXAMPLE_LCD_TOUCH_CONTROLLER_GT911
-        lv_demo_widgets();
+        //lv_demo_widgets();
 #else
         lv_demo_music();
 #endif
         // example_lvgl_demo_ui();
-        // Release the mutex
+         ui_init();
+        //Release the mutex
         lvgl_port_unlock();
     }
 }
